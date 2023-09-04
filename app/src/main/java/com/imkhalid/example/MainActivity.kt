84acec266c489a6518edcf4a417cd804cbc25aa8
Khalid <khalid.saeed@ozoneddigital.com>
2023-09-04 13:26:44 +0500

    updated gradel4.2

---
+++ app/src/main/java/com/imkhalid/example/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.imkhalid.example
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.Toast
+import com.imkhalid.materialdatepicker.date.DatePickerFragmentDialog
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button1).setOnClickListener {
+            val dialog = DatePickerFragmentDialog.newInstance({ view, year, monthOfYear, dayOfMonth ->
+                val calendar = Calendar.getInstance()
+                calendar.set(year, monthOfYear, dayOfMonth)
+
+                val simpleDateFormat = SimpleDateFormat("yyyy-MMM-dd", Locale.getDefault())
+
+                Toast.makeText(applicationContext, simpleDateFormat.format(calendar.time), Toast.LENGTH_SHORT).show()
+            }, 2017, 11, 4)
+            dialog.setMinDate( Date().time)
+            dialog.show(supportFragmentManager, "tag")
+        }
+    }
+}
